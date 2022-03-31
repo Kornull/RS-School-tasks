e30476da996d23a33dff90f31b35f66e4f5f3c41
Kornull <wolod1987kor@gmail.com>
2022-03-31 13:20:18 +0300

    feat:add header

---
+++ shelder/src/main/index.js
@@ -0,0 +1,9 @@
+import '../assets/scss/main.scss';
+import './index.html';
+import './pets.html';
+import '../assets/js/script.js';
+
+
+
+const n = 5 ** 70
+console.log('fffffffffff ' + n,'\n',typeof n );
