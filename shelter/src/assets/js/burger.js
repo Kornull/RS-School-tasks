05afab04b9c2f9653460bc32a2394a6531de26ea
Kornull <wolod1987kor@gmail.com>
2022-04-15 22:16:31 +0300

    feat : add burger click

--- shelter/src/assets/js/burger.js
+++ shelter/src/assets/js/burger.js
@@ -1,1 +1,23 @@
-console.log('hhhhhhhhhhhhhhhhhhhhhhhhh')
+const headerElements = document.querySelector('.header');
+const btnBurger = document.querySelector('.burger')
+const blocksHeader = headerElements.querySelectorAll('div')
+const burgerIcon = headerElements.querySelector('span')
+const nav = headerElements.querySelector('.nav')
+const navLink = nav.querySelectorAll('.nav__link')
+const logoBurger = headerElements.querySelector('.burger__logo')
+if (btnBurger) {
+  btnBurger.addEventListener('click', function (ev) {
+    document.body.classList.toggle('active')
+    nav.classList.toggle('active')
+    blocksHeader.forEach(x => x.classList.toggle('active'));
+    burgerIcon.classList.toggle('active')
+    logoBurger.classList.toggle('active__logo')
+
+  })
+  navLink.forEach(x => x.addEventListener('click', function (el) {
+    document.body.classList.toggle('active')
+    nav.classList.toggle('active')
+    blocksHeader.forEach(x => x.classList.toggle('active'));
+    burgerIcon.classList.toggle('active')
+  }))
+}
--- shelter/src/main/index.js
+++ shelter/src/main/index.js
@@ -2,8 +2,3 @@ import '../assets/scss/main.scss';
 import './index.html';
 import './pets.html';
 import '../assets/js/script.js';
-
-
-
-const n = 5 ** 70
-console.log('fffffffffff ' + n,'\n',typeof n );
