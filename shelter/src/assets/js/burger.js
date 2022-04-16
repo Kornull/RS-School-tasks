1e5c14dc143205e2bc1f91868e8709bbd7d7894c
Kornull <wolod1987kor@gmail.com>
2022-04-17 01:45:48 +0300

    fix burger animation

--- shelter/src/assets/js/burger.js
+++ shelter/src/assets/js/burger.js
@@ -5,35 +5,26 @@ const burgerIcon = headerElements.querySelector('span')
 const nav = headerElements.querySelector('.nav')
 const navLink = nav.querySelectorAll('.nav__link')
 const logoBurger = headerElements.querySelector('.burger__logo')
-let count = 0;
 
-
-const remClass = () => {
-  navLink.forEach(x => x.onclick = (el) => {
-    document.body.classList.remove('active')
-    nav.classList.remove('active')
-    blocksHeader.forEach(x => x.classList.remove('active'));
-    burgerIcon.classList.remove('active')
-    logoBurger.classList.remove('active__logo')
-  })
-};
 
 
 if (btnBurger) {
   btnBurger.onclick = () => {
-    count += 1;
-    console.log(count)
     document.body.classList.toggle('active')
     nav.classList.toggle('active')
     blocksHeader.forEach(x => x.classList.toggle('active'));
     burgerIcon.classList.toggle('active')
     logoBurger.classList.toggle('active__logo')
-    if (count % 2 === 0) {
-      remClass()
-      count = 0;
-    }
-  };
+  }
 }
+navLink.forEach(x => x.onclick = (el) => {
+  document.body.classList.remove('active')
+  nav.classList.remove('active')
+  blocksHeader.forEach(x => x.classList.remove('active'));
+  burgerIcon.classList.remove('active')
+  logoBurger.classList.remove('active__logo')
+})
+
 
 window.addEventListener("orientationchange", function (ev) {
   document.body.classList.remove('active')
