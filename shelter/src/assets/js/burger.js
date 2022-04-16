a165d7a901c01f72db62215a59c84483630a3d40
Kornull <wolod1987kor@gmail.com>
2022-04-16 14:39:58 +0300

    feat: animation burger

--- shelter/src/assets/js/burger.js
+++ shelter/src/assets/js/burger.js
@@ -6,18 +6,26 @@ const nav = headerElements.querySelector('.nav')
 const navLink = nav.querySelectorAll('.nav__link')
 const logoBurger = headerElements.querySelector('.burger__logo')
 if (btnBurger) {
-  btnBurger.addEventListener('click', function (ev) {
+  btnBurger.onclick = () => {
     document.body.classList.toggle('active')
     nav.classList.toggle('active')
     blocksHeader.forEach(x => x.classList.toggle('active'));
     burgerIcon.classList.toggle('active')
     logoBurger.classList.toggle('active__logo')
 
-  })
-  navLink.forEach(x => x.addEventListener('click', function (el) {
+  }
+  navLink.forEach(x => x.onclick = (el) => {
     document.body.classList.toggle('active')
     nav.classList.toggle('active')
     blocksHeader.forEach(x => x.classList.toggle('active'));
     burgerIcon.classList.toggle('active')
-  }))
+    logoBurger.classList.toggle('active__logo')
+  });
 }
+window.addEventListener("orientationchange", function(ev) {
+  document.body.classList.remove('active')
+    nav.classList.remove('active')
+    blocksHeader.forEach(x => x.classList.remove('active'));
+    burgerIcon.classList.remove('active')
+    logoBurger.classList.remove('active__logo')
+});
