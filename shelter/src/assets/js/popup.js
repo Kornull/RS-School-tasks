cede516802e31e6f95aef8d529ced7862ebd65ac
Kornull <wolod1987kor@gmail.com>
2022-04-24 13:29:58 +0300

    fix: popup

--- shelter/src/assets/js/popup.js
+++ shelter/src/assets/js/popup.js
@@ -4,32 +4,37 @@ const popupPets = document.querySelector('.popup--pets');
 const slider = document.querySelector('.pets__slider');
 const popup = document.querySelector('.popup');
 const id = Array.from(document.all).map(i => i.id).filter(i => i != "");
+const arrIdBlock = ['help', 'footer', 'text-our', 'pets']
+const arrId = [];
 
+id.forEach(i => {
+  if (!arrIdBlock.includes(i)) arrId.push(i)
+});
 
 (function () {
   slider.querySelectorAll('.slider__blocks').forEach(el => {
     el.addEventListener('click', function (x) {
-      popup.classList.add('active')
-      let namePet = el.querySelector('h3').textContent
-      console.log(el.querySelector('h3').textContent)
-      for (let i of petsConfig) {
-        if (i.name === namePet) {
-          if (popupPets) {
-            document.querySelector('#img').innerHTML = `<img src="../${i.img}">`
-          } else {
-            document.querySelector('#img').innerHTML = `<img src="${i.img}">`
-          }
-          document.querySelector('#name').innerText = i.name;
-          document.querySelector('#type').innerText = i.type;
-          document.querySelector('#breed').innerText = i.breed;
-          document.querySelector('#description').innerText = i.description;
-          document.querySelector('#age').innerText = i.age;
-          document.querySelector('#diseases').innerText = i.diseases;
-          document.querySelector('#inoculations').innerText = i.inoculations;
-          document.querySelector('#parasites').innerText = i.parasites;
-          console.log(i)
-        }
-      }
+      let namePet = el.querySelector('h3').textContent;
+      popup.classList.add('active');
+      openPopup(namePet)
     });
   })
 }())
+
+const openPopup = (el) => {
+  petsConfig.forEach(j => {
+    if (el === j.name) {
+      arrId.forEach(i => {
+        if (i === 'img') {
+          if (popupPets) {
+            popup.querySelector(`#${i}`).innerHTML = `<img src="../${j[i]}">`;
+          } else {
+            popup.querySelector(`#${i}`).innerHTML = `<img src="${j[i]}">`;
+          }
+        } else {
+          popup.querySelector(`#${i}`).innerText = j[i];
+        }
+      });
+    }
+  });
+};
