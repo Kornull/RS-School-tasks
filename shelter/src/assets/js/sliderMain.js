f2faccaabb2860ef8650b1655971e3ed137bf3de
Kornull <wolod1987kor@gmail.com>
2022-04-26 00:03:56 +0300

    feat:add slider width< 1280px

--- shelter/src/assets/js/sliderMain.js
+++ shelter/src/assets/js/sliderMain.js
@@ -1,10 +1,10 @@
 import cardsObj from '../../main/pets.json';
 import './popup.js'
 import { popurRun } from './popup.js';
+const item = document.querySelectorAll('.item');
 const BTN_LEFT = document.querySelector(".btn__left");
 const BTN_RIGHT = document.querySelector(".btn__right");
 const CAROUSEL = document.querySelector("#slider__carousel");
-const ITEM_ACTIVE = document.querySelector('#item-active');
 const ITEM_LEFT = document.querySelector("#item-left");
 const ITEM_RIGHT = document.querySelector("#item-right");
 const namesPets = ['sophia', 'timmy', 'scarlet', 'freddie', 'charly', 'woody', 'jennifer', 'katrine']
@@ -12,15 +12,37 @@ let arrCards = [];
 let arr = [];
 let num = 0;
 let count = 0;
-ITEM_ACTIVE.querySelectorAll('div').forEach(x => { if (x['id'] !== '') arr.push(x['id']) })
+let numCards = 0;
+
+const lengthItem = (numCards) => item.forEach((x) => {
+  x.querySelectorAll('.slider__block').forEach((i, l) => {
+    if (l >= numCards) i.remove('div');
+  });
+});
+
+const widthWindow = () => {
+  let size = window.innerWidth;
+  if (size > 1279) numCards = 3;
+  if (size <= 1279) {
+    numCards = 2;
+    lengthItem(numCards)
+  };
+  if (size <= 767) {
+    numCards = 1;
+    lengthItem(numCards)
+
+  };
+}
+setInterval(widthWindow, 100)
+
 const newCard = () => {
-  if (count === 3) {
+  if (count === numCards) {
     count = 0;
     arr = arrCards.slice();
     arrCards = [];
   }
   const card = document.createElement('div')
-  while (arrCards.length < 3) {
+  while (arrCards.length < numCards) {
     num = Math.floor(Math.random() * 8)
     if (!arr.includes(namesPets[num]) && !arrCards.includes(namesPets[num])) {
       arrCards.push(namesPets[num])
@@ -33,7 +55,7 @@ const newCard = () => {
     const card_text = document.createElement("div");
     const btn = document.createElement('button');
     const title = document.createElement('h3');
-    if (count < 3) {
+    if (count < numCards) {
       card.classList.add('slider__block');
       if (arrCards[count] === j['name'].toLowerCase()) {
         card.classList.add("slider__block");
@@ -91,8 +113,9 @@ CAROUSEL.addEventListener("animationend", (ev) => {
   BTN_LEFT.addEventListener("click", moveLeft);
   BTN_RIGHT.addEventListener("click", moveRight);
   itemChange.innerHTML = '';
+  widthWindow()
 
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < numCards; i++) {
     const card = newCard();
     itemChange.appendChild(card)
     popurRun()
