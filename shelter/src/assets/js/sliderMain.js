6521986eb5df5af704d613dd9393aabd8d6e31d0
Kornull <wolod1987kor@gmail.com>
2022-04-25 14:53:10 +0300

    feat: add slider main-page

--- shelter/src/assets/js/sliderMain.js
+++ shelter/src/assets/js/sliderMain.js
@@ -1,52 +1,102 @@
-const petsNames = ['Katrine', 'Charly', 'Freddie', 'Jennifer', 'Scarlet', 'Sophia', 'Timmy', 'Woody']
-const sliderMain = document.querySelector('.slider__cards')
-const sliderPets = document.querySelector('.slider__cards--our')
-
-function slide(count) {
-  let numName;
-  sliderMain.innerHTML = '';
-  const arrName = [];
-  while (arrName.length < count) {
-    numName = Math.floor(Math.random() * petsNames.length);
-    if (!arrName.includes(numName)) {
-      arrName.push(numName);
+import cardsObj from '../../main/pets.json';
+import './popup.js'
+import { popurRun } from './popup.js';
+const BTN_LEFT = document.querySelector(".btn__left");
+const BTN_RIGHT = document.querySelector(".btn__right");
+const CAROUSEL = document.querySelector("#slider__carousel");
+const ITEM_ACTIVE = document.querySelector('#item-active');
+const ITEM_LEFT = document.querySelector("#item-left");
+const ITEM_RIGHT = document.querySelector("#item-right");
+const namesPets = ['sophia', 'timmy', 'scarlet', 'freddie', 'charly', 'woody', 'jennifer', 'katrine']
+let arrCards = [];
+let arr = [];
+let num = 0;
+let count = 0;
+ITEM_ACTIVE.querySelectorAll('div').forEach(x => { if (x['id'] !== '') arr.push(x['id']) })
+const newCard = () => {
+  if (count === 3) {
+    count = 0;
+    arr = arrCards.slice();
+    arrCards = [];
+  }
+  const card = document.createElement('div')
+  while (arrCards.length < 3) {
+    num = Math.floor(Math.random() * 8)
+    if (!arr.includes(namesPets[num]) && !arrCards.includes(namesPets[num])) {
+      arrCards.push(namesPets[num])
     }
   }
-  for (num of arrName) {
-    let a = document.createElement('li');
-    if (sliderPets) {
-      a.className = 'slider__blocks slider--our';
-    } else {
-      a.className = 'slider__blocks';
+
+  for (let j of cardsObj) {
+    const card_img = document.createElement("div");
+    const img = document.createElement("img");
+    const card_text = document.createElement("div");
+    const btn = document.createElement('button');
+    const title = document.createElement('h3');
+    if (count < 3) {
+      card.classList.add('slider__block');
+      if (arrCards[count] === j['name'].toLowerCase()) {
+        card.classList.add("slider__block");
+        btn.classList.add('btn', 'btn__white');
+        btn.innerText = 'Learn more';
+        card_text.classList.add('slider__title');
+        title.classList.add('our__pets--slider');
+        img.src = j.img;
+        card_img.classList.add('slider__img');
+        title.innerText = j.name;
+        card_text.appendChild(title);
+        card_img.appendChild(img)
+        card.appendChild(card_img);
+        card.appendChild(card_text);
+        card.appendChild(btn);
+        card.id = `${j.name.toLowerCase()}`
+
+        count++
+        return card;
+
+      }
     }
-    a.innerHTML = `<div class='slider__img'><img src="../assets/img/pets-${petsNames[num].toLowerCase()}.png"></img></div><div class="slider__title">
-  <h3 class="our__pets--slider">${petsNames[num]}</h3>
-  </div> <button class="btn btn__white">
-   Learn more
-  </button>`;
-    sliderMain.append(a);
   }
 }
 
-window.addEventListener('resize', function () {
-  let size = document.documentElement.clientWidth;
-  if (!sliderPets) {
-    if (size > 1279) {
-      count = 3;
-    } else if (size > 767) {
-      count = 2;
-    } else {
-      count = 1;
-    }
+const moveLeft = () => {
+  CAROUSEL.classList.add("transition-left");
+  BTN_RIGHT.removeEventListener("click", moveRight);
+  BTN_LEFT.removeEventListener("click", moveLeft);
+};
+
+const moveRight = () => {
+  CAROUSEL.classList.add("transition-right");
+  BTN_LEFT.removeEventListener("click", moveLeft);
+  BTN_RIGHT.removeEventListener("click", moveRight);
+};
+
+BTN_LEFT.addEventListener("click", moveLeft);
+
+
+BTN_RIGHT.addEventListener("click", moveRight);
+
+CAROUSEL.addEventListener("animationend", (ev) => {
+  let itemChange;
+  if (ev["animationName"] === "move-left") {
+    CAROUSEL.classList.remove("transition-left");
+    itemChange = ITEM_LEFT;
+    document.querySelector("#item-active").innerHTML = ITEM_LEFT.innerHTML;
   }
-  if (sliderPets) {
-    if (size > 1279) {
-      count = 8;
-    } else if (size > 767) {
-      count = 6;
-    } else {
-      count = 3;
-    }
+  else {
+    CAROUSEL.classList.remove("transition-right");
+    itemChange = ITEM_RIGHT;
+    document.querySelector("#item-active").innerHTML = ITEM_RIGHT.innerHTML;
   }
-  slide(count)
+  BTN_LEFT.addEventListener("click", moveLeft);
+  BTN_RIGHT.addEventListener("click", moveRight);
+  itemChange.innerHTML = '';
+
+  for (let i = 0; i < 3; i++) {
+    const card = newCard();
+    itemChange.appendChild(card)
+    popurRun()
+  }
 });
+
+
