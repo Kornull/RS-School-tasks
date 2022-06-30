246496e7bf570fe2e733db716165be69d365fa5d
Kornull <wolod1987kor@gmail.com>
2022-07-01 00:59:48 +0300

    refactor: add typing sources

--- news-JS-main/src/components/view/sources/sources.ts
+++ news-JS-main/src/components/view/sources/sources.ts
@@ -1,21 +1,21 @@
 import './sources.css';
-
+import { GetSources } from '../../types/index';
 class Sources {
-    draw(data) {
-        const fragment = document.createDocumentFragment();
-        const sourceItemTemp = document.querySelector('#sourceItemTemp');
+  draw(data: GetSources[]): void {
+    const fragment: DocumentFragment = document.createDocumentFragment();
+    const sourceItemTemp = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
 
-        data.forEach((item) => {
-            const sourceClone = sourceItemTemp.content.cloneNode(true);
+    data.forEach((item) => {
+      const sourceClone = sourceItemTemp.content.cloneNode(true) as Element;
 
-            sourceClone.querySelector('.source__item-name').textContent = item.name;
-            sourceClone.querySelector('.source__item').setAttribute('data-source-id', item.id);
+      (<HTMLElement>sourceClone.querySelector('.source__item-name')).textContent = item.name;
+      (<HTMLTemplateElement>sourceClone.querySelector('.source__item')).setAttribute('data-source-id', item.id);
 
-            fragment.append(sourceClone);
-        });
-
-        document.querySelector('.sources').append(fragment);
-    }
+      fragment.append(sourceClone);
+    });
+    const newsBtn = document.querySelector('.sources') as HTMLDivElement;
+    newsBtn.append(fragment);
+  }
 }
 
 export default Sources;
