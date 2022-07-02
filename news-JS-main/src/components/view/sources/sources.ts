7e1d756d4529b37e7a04073c014bc96de6d776e8
Kornull <wolod1987kor@gmail.com>
2022-07-02 11:50:13 +0300

    refacor: add types

--- news-JS-main/src/components/view/sources/sources.ts
+++ news-JS-main/src/components/view/sources/sources.ts
@@ -1,11 +1,11 @@
 import './sources.css';
 import { GetSources } from '../../types/index';
 class Sources {
-  draw(data: GetSources[]): void {
+  public draw(data: GetSources[]): void {
     const fragment: DocumentFragment = document.createDocumentFragment();
     const sourceItemTemp = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
 
-    data.forEach((item) => {
+    data.forEach((item): void => {
       const sourceClone = sourceItemTemp.content.cloneNode(true) as Element;
 
       (<HTMLElement>sourceClone.querySelector('.source__item-name')).textContent = item.name;
