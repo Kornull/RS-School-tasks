660464a1ca57c0b220561a4dd7cf537124067a15
Kornull <wolod1987kor@gmail.com>
2022-07-02 11:44:55 +0300

    fix: del interface

--- news-JS-main/src/components/app/app.ts
+++ news-JS-main/src/components/app/app.ts
@@ -1,35 +1,27 @@
 import AppController from '../controller/controller';
+import { IContentNews, IContentSources } from '../types';
 import AppView from '../view/appView';
 
-interface GetSource {
-  status: string;
-  sources: object[];
-  totalResults?: number;
-  articles?: object[];
-}
-
 class App {
-  controller: AppController;
-  view: AppView;
+  private controller: AppController;
+  private view: AppView;
   constructor() {
     this.controller = new AppController();
     this.view = new AppView();
   }
 
-  public start() {
+  public start(): void {
     const el = document.querySelector('.sources') as HTMLElement | null;
     if (el !== null)
       el.addEventListener('click', (e: MouseEvent) =>
-        this.controller.getNews(e, (data: GetSource) => {
+        this.controller.getNews(e, (data: IContentNews) => {
           this.view.drawNews(data);
         })
       );
-    this.controller.getSources((data: GetSource) => {
-      // console.log(data)
+    this.controller.getSources((data: IContentSources) => {
       this.view.drawSources(data);
     });
   }
 }
 
 export default App;
-
