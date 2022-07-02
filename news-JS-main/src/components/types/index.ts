ea49e7cf5cfba7f20842c8983ce2f2889da73027
Kornull <wolod1987kor@gmail.com>
2022-07-02 11:43:43 +0300

    feat:add interfaces/types

--- news-JS-main/src/components/types/index.ts
+++ news-JS-main/src/components/types/index.ts
@@ -32,3 +32,13 @@ export interface IContentSources {
   status: string;
   sources: GetSources[];
 }
+
+export type Key = {
+  [key: string]: string;
+};
+export type Callback<T> = (data: T) => void;
+
+export interface IResponseLoader {
+  endpoint: string;
+  options?: { sources: string };
+}
