54ee90fcd37e41702254369bff6beaa56a07aa2c
Kornull <wolod1987kor@gmail.com>
2022-07-02 11:46:29 +0300

    refacor: add types

--- news-JS-main/src/components/controller/controller.ts
+++ news-JS-main/src/components/controller/controller.ts
@@ -1,39 +1,40 @@
 import AppLoader from './appLoader';
+import { IContentNews, IContentSources, Callback } from '../types';
 
 class AppController extends AppLoader {
-    getSources(callback) {
-        super.getResp(
+  public getSources(callback: Callback<IContentSources>): void {
+    super.getResp(
+      {
+        endpoint: 'sources',
+      },
+      callback
+    );
+  }
+
+  public getNews(e: MouseEvent, callback: Callback<IContentNews>): void {
+    let target = e.target as HTMLDivElement;
+    const newsContainer = e.currentTarget as HTMLDivElement;
+
+    while (target !== newsContainer) {
+      if (target.classList.contains('source__item')) {
+        const sourceId = target.getAttribute('data-source-id') as string;
+        if (newsContainer.getAttribute('data-source') !== sourceId) {
+          newsContainer.setAttribute('data-source', sourceId);
+          super.getResp(
             {
-                endpoint: 'sources',
+              endpoint: 'everything',
+              options: {
+                sources: sourceId,
+              },
             },
             callback
-        );
-    }
-
-    getNews(e, callback) {
-        let target = e.target;
-        const newsContainer = e.currentTarget;
-
-        while (target !== newsContainer) {
-            if (target.classList.contains('source__item')) {
-                const sourceId = target.getAttribute('data-source-id');
-                if (newsContainer.getAttribute('data-source') !== sourceId) {
-                    newsContainer.setAttribute('data-source', sourceId);
-                    super.getResp(
-                        {
-                            endpoint: 'everything',
-                            options: {
-                                sources: sourceId,
-                            },
-                        },
-                        callback
-                    );
-                }
-                return;
-            }
-            target = target.parentNode;
+          );
         }
+        return;
+      }
+      target = target.parentNode as HTMLDivElement;
     }
+  }
 }
 
 export default AppController;
