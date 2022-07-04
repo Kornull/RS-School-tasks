ba1e638dd091be6fc274165177b63e3a37d0a813
Kornull <wolod1987kor@gmail.com>
2022-07-04 23:16:34 +0300

    fix: del comment

--- news-JS-main/src/components/controller/appLoader.ts
+++ news-JS-main/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
   constructor() {
     super('https://newsapi.org/v2/', {
-      apiKey: 'f522a8c79f87426cb4ad5026d012501a', // получите свой ключ https://newsapi.org/
+      apiKey: 'f522a8c79f87426cb4ad5026d012501a',
     });
   }
 }
