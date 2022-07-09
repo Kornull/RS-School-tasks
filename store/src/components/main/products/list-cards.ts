ca6c9d14c20e6f882d392a99c96ed4f7f4f9f18e
Kornull <wolod1987kor@gmail.com>
2022-07-09 23:03:10 +0300

    feat: add cards loader

---
+++ store/src/components/main/products/list-cards.ts
@@ -0,0 +1,36 @@
+import { Laptop } from '../../types';
+import employee from '../../../laptop.json';
+
+class Cards {
+  createCard(): DocumentFragment {
+    const fragnent: DocumentFragment = document.createDocumentFragment();
+    // card.className = 'card_list';
+    employee.forEach((_el: Laptop) => {
+      const card = document.createElement('div');
+      const img = document.createElement('img');
+      const ul = document.createElement('ul');
+      card.className = 'card';
+      img.className = 'card__img';
+      ul.className = 'card__list';
+      card.innerHTML = _el.model;
+      img.src = `./assets/img/${_el.image}`;
+      for (const i of _el.description) {
+        const li = document.createElement('li');
+        const arrK = Object.keys(i);
+        const arrV = Object.values(i);
+        li.innerHTML = `${arrK[0]} - ${arrV[0]}`;
+        ul.appendChild(li);
+      }
+
+      const year = document.createElement('div');
+      year.innerHTML = `Year - ${_el.year}`;
+      card.appendChild(img);
+      card.appendChild(ul);
+      card.appendChild(year);
+      fragnent.append(card);
+    });
+    return fragnent;
+  }
+}
+
+export default Cards;
--- store/src/components/main/products/product.ts
+++ store/src/components/main/products/product.ts
@@ -1,7 +1,14 @@
+import Cards from './list-cards';
+
 class Products {
+  createCard: Cards;
+  constructor() {
+    this.createCard = new Cards();
+  }
   create() {
     const product: HTMLDivElement = document.createElement('div');
     product.className = 'main__product';
+    product.appendChild(this.createCard.createCard());
     return product;
   }
 }
