04a4bf8471c0322b40e805380428137896bba3f8
Kornull <wolod1987kor@gmail.com>
2022-07-11 18:19:53 +0300

    feat: add filter by brand

--- store/src/components/main/filters/filter.ts
+++ store/src/components/main/filters/filter.ts
@@ -1,41 +1,84 @@
 import employee from '../../../laptop.json';
 import { Laptop } from '../../types';
 import Products from '../products/product';
-class Filters {
-  prod: Products;
+class Filters extends Products {
+  filter: HTMLDivElement;
+  brandArr: string[];
+  newSorArr: string[];
+  fragment: DocumentFragment;
+  sortArr: Laptop[];
   constructor() {
-    this.prod = new Products();
+    super();
+    this.filter = document.createElement('div');
+    this.brandArr = [];
+    this.newSorArr = [];
+    this.fragment = document.createDocumentFragment();
+    this.sortArr = [];
   }
   filterName(): HTMLDivElement {
-    const main = document.querySelector('main__product') as Element;
-    console.log('main', main);
-    const filterForName: HTMLDivElement = document.createElement('div');
-    filterForName.className = 'filter__name';
+    this.filter.className = 'filter__name';
     const input = document.createElement('input');
     input.type = 'text';
     input.id = 'search';
 
     input.addEventListener('input', () => {
-      let sortArr: Laptop[] = [];
+      this.sortArr = [];
       if (input.value.length === 0) {
-        this.prod.createCard(employee);
-        sortArr = [];
+        this.createCard(employee);
+        this.sortArr = [];
       } else {
-        employee.forEach((el) => {
-          if (el.brand.toLowerCase().includes(input.value)) {
-            sortArr.push(el);
-            console.log(el.brand);
-
-            this.prod.createCard(sortArr);
-          }
-        });
+        this.newSortArr(input.value);
       }
     });
 
-    filterForName.appendChild(input);
+    this.filter.appendChild(input);
 
-    return filterForName;
+    return this.filter;
+  }
+
+  filterBrand() {
+    this.filter = document.createElement('div');
+    this.filter.className = 'filter__brand';
+
+    employee.forEach((el) => {
+      if (!this.brandArr.includes(el.brand)) {
+        this.brandArr.push(el.brand);
+      }
+    });
+    this.brandArr.forEach((el) => {
+      const btn: HTMLButtonElement = document.createElement('button');
+      btn.className = 'btn';
+      btn.innerText = el;
+      btn.addEventListener('click', () => {
+        this.newSortArr(btn.innerText);
+      });
+      this.fragment.append(btn);
+    });
+
+    this.filter.appendChild(this.fragment);
+    return this.filter;
+  }
+
+  newSortArr(a: string) {
+    a = a.toLowerCase();
+    // this.brandArr = [];
+    if (!this.newSorArr.includes(a)) {
+      this.newSorArr.push(a);
+      employee.forEach((el) => {
+        if (el.brand.toLowerCase().includes(a)) {
+          this.sortArr.push(el);
+          this.createCard(this.sortArr);
+        }
+      });
+    } else {
+      this.newSorArr.splice(this.newSorArr.indexOf(a), 1);
+      this.sortArr = this.sortArr.filter((x) => x.brand.toLowerCase() !== a);
+      if (this.sortArr.length !== 0) {
+        this.createCard(this.sortArr);
+      } else {
+        this.createCard(employee);
+      }
+    }
   }
 }
-
 export default Filters;
--- store/src/components/main/search/search.ts
+++ store/src/components/main/search/search.ts
@@ -9,6 +9,7 @@ class Search {
     const search = document.createElement('div');
     search.className = 'main__filter filter';
     search.appendChild(this.nameFilter.filterName());
+    search.appendChild(this.nameFilter.filterBrand());
     return search;
   }
 }
