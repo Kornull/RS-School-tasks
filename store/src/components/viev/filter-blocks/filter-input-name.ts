9f838d2e4f8a190fb1b03dc39a9d2d160dc44188
Kornull <wolod1987kor@gmail.com>
2022-07-17 00:07:34 +0300

    fix: local

--- store/src/components/viev/filter-blocks/filter-input-name.ts
+++ store/src/components/viev/filter-blocks/filter-input-name.ts
@@ -1,9 +1,14 @@
-// import employee from '../../../laptop.json';
+import employee from '../../../laptop.json';
 import FilterBlocksDiv from './filters-blocks';
-
+import SortedCard from '../../controller/sorted';
+import { LocalStor } from '../../controller/storage/storage';
 class InputName extends FilterBlocksDiv {
+  local: LocalStor | undefined;
+  sortedCards: SortedCard | undefined;
   constructor() {
     super();
+    this.local = new LocalStor();
+    this.sortedCards = new SortedCard();
   }
 
   filterName(): HTMLDivElement {
@@ -18,7 +23,46 @@ class InputName extends FilterBlocksDiv {
     input.placeholder = 'enter brand';
     label.setAttribute('for', input.id);
     label.innerText = 'Brand';
+    input.addEventListener('keyup', () => {
+      let countSort: number[] | string[] = [];
+      let count = 0;
+      let btnId: string[] = [];
 
+      employee.forEach((el) => {
+        if (el.brand.toLowerCase().includes(input.value.toLowerCase())) {
+          count = 1;
+          btnId.push(el.brand);
+        }
+      });
+      if (count === 0) {
+        btnId = [''];
+      }
+      btnId = Array.from(new Set(btnId));
+      if (this.local !== undefined) {
+        this.local.set('ValueInput', [input.value]);
+        this.local.set('BtnInputId', btnId);
+        countSort = [...this.local.get('CountSortedGet')];
+        if (btnId.length === 0) {
+          countSort = countSort.filter((e) => e !== '4');
+        } else {
+          countSort.push('4');
+        }
+        if (input.value.length === 0) {
+          this.local.set('ValueInput', []);
+          this.local?.set('BtnInputId', []);
+          countSort = countSort.filter((e) => e !== '4');
+        }
+        countSort = Array.from(new Set(countSort));
+        this.local?.set('CountSortedGet', countSort);
+      }
+
+      if (this.sortedCards !== undefined) {
+        this.sortedCards.newSortArr();
+      }
+    });
+    if (this.local !== undefined) {
+      input.value = this.local.get('ValueInput')[0];
+    }
     form.append(label);
     form.append(input);
     this.filterSearch.appendChild(form);
