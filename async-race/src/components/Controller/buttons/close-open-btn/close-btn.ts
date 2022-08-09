2708e0316022c5015eaeba91789dc146641d560f
Kornull <wolod1987kor@gmail.com>
2022-08-09 13:00:31 +0300

    fix: start btn

--- async-race/src/components/Controller/buttons/close-open-btn/close-btn.ts
+++ async-race/src/components/Controller/buttons/close-open-btn/close-btn.ts
@@ -13,9 +13,10 @@ export const openAllBtns = () => {
     if (!btn.classList.contains('btn__race-stop') && !btn.classList.contains('btn__update-car')) {
       if (!btn.classList.contains('btn__race-all--car')) {
         btn.removeAttribute('disabled');
-        if (btn.classList.contains('close')) btn.classList.remove('close');
+
       }
     }
+    if (btn.classList.contains('close')) btn.classList.remove('close');
     if (btn.classList.contains('btn__race-stop')) btn.setAttribute('disabled', 'disabled');
   });
 };
